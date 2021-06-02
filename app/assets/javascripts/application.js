9c887d7ae18f6b976a302b9aa82669645af71af6
Boris Parusnikov <2669997@gmail.com>
2021-06-02 23:19:32 +0300

    coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery3
+
 
 $(function() {
   console.log('Document is loaded');
---
+++ app/channels/room_channel.rb
@@ -0,0 +1,18 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+
+    @room = Room.find(params[:roomId])
+
+    stream_from "room_channel_#{@room.id}"
+  end
+
+  def unsubscribed
+    logger.info "Unsubscribed to RoomChannel"
+  end
+
+  def speak(data)
+    logger.info "RoomChannel, speak: #{data.inspect}"
+
+    ActionCable.server.broadcast "room_channel_#{@room.id}", message: data['message']
+end
