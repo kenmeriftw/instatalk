671d94e9d592a074b4493579a19e4047740ebc84
Boris Parusnikov <2669997@gmail.com>
2021-06-02 23:32:02 +0300

    services added

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -5,14 +5,22 @@ class RoomChannel < ApplicationCable::Channel
     @room = Room.find(params[:roomId])
 
     stream_from "room_channel_#{@room.id}"
+
+    speak('message' => '* * * joined the room * * *')
   end
 
   def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
     logger.info "Unsubscribed to RoomChannel"
+
+    speak('message' => '* * * left the room * * *')
   end
 
   def speak(data)
     logger.info "RoomChannel, speak: #{data.inspect}"
 
-    ActionCable.server.broadcast "room_channel_#{@room.id}", message: data['message']
+    MessageService.new(
+      body: data['message'], room: @room, user: current_user
+    ).perform
+  end
 end
